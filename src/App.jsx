212fb6303499913b946850cf3985ef54a589bc14
Bhanushri2197 <bhanushri2197@gmail.com>
2024-08-19 01:28:30 +0530

    app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.js"
-import AddTodoForm from './AddTodoForm'
-import TodoList from './TodoList'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import AddTodoForm from './AddTodoForm';
+import TodoList from './TodoList';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [toDoLists,settoDoList] = useState([])
-  const fetchData = async () => {
-      try {
-          const todoData = await axios.get(`https://66ba6aaafa763ff550fbbd5d.mockapi.io/todolist`)
-          settoDoList(todoData.data)
-      } catch (error) {
-          alert("Something went wrong")
-      }
-  }
-  useEffect(() => {
-      fetchData()
-  },[])
+  const [toDoLists, setToDoList] = useState([]);
 
-  return <div className='toDoBlock'>
+  // Function to fetch data from API
+  const fetchData = async () => {
+    try {
+      const todoData = await axios.get('https://66ba6aaafa763ff550fbbd5d.mockapi.io/todolist');
+      setToDoList(todoData.data);
+    } catch (error) {
+      alert("Something went wrong");
+    }
+  };
+
+  // Fetch data on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Callback function to update todo list
+  const handleAddTodo = async () => {
+    await fetchData();
+  };
+
+  return (
+    <div className='toDoBlock'>
       <div className="container">
         <h1 className="headingTitle text-center mt-5 mb-5">My Todo</h1>
-         <AddTodoForm/>
+        <AddTodoForm onAddTodo={handleAddTodo} />
 
-         <div className='row'>
-                {
-                    toDoLists.map((todoValues) => {
-                        return <TodoList todoValues={todoValues} />
-                    })
-                }
-          </div>
+        <div className='row justify-content-center'>
+          {toDoLists.map((todoValues) => (
+            <TodoList  onUpdate={fetchData} key={todoValues.id} todoValues={todoValues} />
+          ))}
+        </div>
       </div>
        
-  </div>
-    
-  }
-  
+    </div>
+  );
+}
 
-export default App
+export default App;
